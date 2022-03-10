e83ae01243816f255653bd6e44c557647c946ac7
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-03-10 16:08:05 +0100

    reduce

--- sommaLista.js
+++ sommaLista.js
@@ -11,32 +11,47 @@
 
 
 // **************************** 
-function leggiESomma(valore, accumulatore, i ){
-    accumulatore +=  valore; // somma
-    console.log(`sono dentro a tmp valore i=${i} acc=${accumulatore} `);
-    return accumulatore
-}
 
-function somma(listaPassata, secondoPar){
-    console.log(`sono dentro a somma, listaPassata= ${listaPassata} `);
+function ciclaLista(funzDaPassare, listaPassata){
+   //  console.log(`sono dentro a somma, listaPassata= ${listaPassata} `);
     let accumulatore = 0;
 
-    if (!listaPassata.length) return listaPassata + secondoPar;
-    // if (typeof(listaPassata) === 'number')   listaPassata + secondoPar;
+    // caro for ciclami listaPassata e chiama funzDaPassare, con risultato sul accumulatore
     for(let i=0; i<listaPassata.length; i++){
-        leggiESomma(listaPassata[i], accumulatore, i );
-        console.log(`sono dentro al for valore valore i=${i} acc=${accumulatore} `);
+        // qui chiamo La funzione passata per fare il calcolo
+        accumulatore = funzDaPassare(listaPassata[i], accumulatore, i );
+        // console.log(`sono dentro al for valore valore i=${i} acc=${accumulatore} `);
     }
 
     return accumulatore;
 }
 
-
-
 // Qui sotto chiamo la stessa funzione più volte passando parametri diversi.
 
-const risultato = somma([1,2,3], 2);
+function somma(valore, accumulatore, i ){
+    return accumulatore + valore
+}
+
+const moltiplica = (a, b) => a * b;
+const dividi = (a, b) => a / b;
+const algebra = (a, b) => (a * a) + (b * b);
+
+const risultato = ciclaLista( somma, [1,2,3] );
 console.log('sono dopo la chiamata alla funzione risultato=', risultato);
+
+const risultato2 = ciclaLista( moltiplica, [1,2,3] );
+console.log('sono dopo la chiamata alla funzione risultato=', risultato2);
+
+const risultato3 = ciclaLista( dividi, [1,2,3] );
+console.log('sono dopo la chiamata alla funzione risultato=', risultato3);
+
+
+const risultato4 = ciclaLista( algebra, [1,2,3] );
+console.log('sono dopo la chiamata alla funzione risultato=', risultato4);
+// console.log( 'REDUCE !!!',  [1,2,3].reduce(algebra, 0));
+
+
+
 // const risultato2 = somma([1,2,3]);
 // console.log('sono dopo la chiamata alla funzione risultato=', risultato2);
 // somma([10,40, 50]);
